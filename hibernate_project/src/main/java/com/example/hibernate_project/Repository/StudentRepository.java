f479184ed336084314aa2257306a03fc6fe3ba5b
archita <deepg9997@gmail.com>
2024-10-01 21:06:09 +0530

    dynamic queries in StudentRepository

--- hibernate_project/src/main/java/com/example/hibernate_project/Repository/StudentRepository.java
+++ hibernate_project/src/main/java/com/example/hibernate_project/Repository/StudentRepository.java
@@ -9,18 +9,38 @@ import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
+    //Dynamic Query Methods
     public List<Student> findByname(String name);
     public List<String> findByemail(String email);
     public List<String> findByage(Integer age);
     public List<Student> findByAGeGreaterThan(Integer age);
 
+    // Find by email containing certain text (like)
     List<Student> findByemailContaining(String email);
+
+    // Find by age between certain values
     List<Student> findByAgeBetween(int startAge, int endAge);
 
     Long countByAge(Integer age);
+
+    // Count students with marks greater than a specific value
     Long countByAgeGreaterThan(Integer age);
+
+    // Count students of a certain age range
     Long countByAgeBetween(Integer startAge, Integer endAge);
 
+    // Find SUM of age
     @Query("SELECT SUM(s.age) FROM Student s")
     Integer findSumofAge();
+
+    // Find MIN (minimum) age
+    @Query("SELECT MIN(s.age) FROM Student s")
+    Integer findMinAge();
+
+    // Find MAX (maximum) age
+    @Query("SELECT MAX(s.age) FROM Student s")
+    Integer findMaxAge();
+
+    // Find Top 3 students with the highest age
+    List<Student> findTop3ByOrderByAgeDesc();
 }
